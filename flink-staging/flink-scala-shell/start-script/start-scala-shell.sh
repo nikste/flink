c3ffd1aa074dc11d4fb95c1b161e7b82b1e89b25
fleischhauf <fleischhauf@hotmail.com>
2015-04-27 10:54:27 +0200

    Added help function to scala shell startup .sh

--- flink-staging/flink-scala-shell/start-script/start-scala-shell.sh
+++ flink-staging/flink-scala-shell/start-script/start-scala-shell.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-java -cp ../lib/flink-dist-0.9-SNAPSHOT.jar org.apache.flink.api.scala.FlinkShell -host "localhost" -port 1000
 
+if [ "$1" == "-h" ]; then
+  echo "Usage:"
+  echo " -h -help shows this message"
+  echo " -h -host <hostname> specifies remote host"
+  echo " -p -port <portnum> specifies the port for remote host"
+  exit 0
+fi
+
+
+java -cp ../lib/flink-dist-0.9-SNAPSHOT.jar org.apache.flink.api.scala.FlinkShell $1 $2 $3 $4
 
 
