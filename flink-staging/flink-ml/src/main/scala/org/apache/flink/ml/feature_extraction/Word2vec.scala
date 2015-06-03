612794c19fa7aa645d0b4335506f828b0b02d13d
Nikolaas Steenbergen <Nikolaas.steenbergen@googlemail.com>
2015-06-03 12:15:17 +0200

    added spark seed function for comparison

--- flink-staging/flink-ml/src/main/scala/org/apache/flink/ml/feature_extraction/Word2vec.scala
+++ flink-staging/flink-ml/src/main/scala/org/apache/flink/ml/feature_extraction/Word2vec.scala
@@ -35,6 +35,132 @@ import org.apache.flink.util.Collector
 import scala.collection.mutable
 import scala.collection.mutable.{ListBuffer, ArrayBuilder}
 import scala.reflect.ClassTag
+
+
+
+
+
+import java.nio.ByteBuffer
+import java.util.{Random => JavaRandom}
+
+import scala.util.hashing.MurmurHash3
+
+
+/**
+ * This class implements a XORShift random number generator algorithm
+ * Source:
+ * Marsaglia, G. (2003). Xorshift RNGs. Journal of Statistical Software, Vol. 8, Issue 14.
+ * @see <a href="http://www.jstatsoft.org/v08/i14/paper">Paper</a>
+ * This implementation is approximately 3.5 times faster than
+ * { java.util.Random java.util.Random}, partly because of the algorithm, but also due
+ * to renouncing thread safety. JDK's implementation uses an AtomicLong seed, this class
+ * uses a regular Long. We can forgo thread safety since we use a new instance of the RNG
+ * for each thread.
+ */
+private class XORShiftRandom(init: Long) extends JavaRandom(init) {
+  
+  def this() = this(System.nanoTime)
+
+  private var seed = XORShiftRandom.hashSeed(init)
+
+  // we need to just override next - this will be called by nextInt, nextDouble,
+  // nextGaussian, nextLong, etc.
+  override protected def next(bits: Int): Int = {
+    var nextSeed = seed ^ (seed << 21)
+    nextSeed ^= (nextSeed >>> 35)
+    nextSeed ^= (nextSeed << 4)
+    seed = nextSeed
+    (nextSeed & ((1L << bits) -1)).asInstanceOf[Int]
+  }
+
+  override def setSeed(s: Long) {
+    seed = XORShiftRandom.hashSeed(s)
+  }
+}
+
+/** Contains benchmark method and main method to run benchmark of the RNG */
+private object XORShiftRandom {
+
+  /** Hash seeds to have 0/1 bits throughout. */
+  private def hashSeed(seed: Long): Long = {
+    val bytes = ByteBuffer.allocate(java.lang.Long.SIZE).putLong(seed).array()
+    MurmurHash3.bytesHash(bytes)
+  }
+
+  /**
+   * Main method for running benchmark
+   * @param args takes one argument - the number of random numbers to generate
+   */
+  def main(args: Array[String]): Unit = {
+    if (args.length != 1) {
+      println("Benchmark of XORShiftRandom vis-a-vis java.util.Random")
+      println("Usage: XORShiftRandom number_of_random_numbers_to_generate")
+      System.exit(1)
+    }
+    println(benchmark(args(0).toInt))
+  }
+  /**
+   * Method executed for repeating a task for side effects.
+   * Unlike a for comprehension, it permits JVM JIT optimization
+   */
+  def times(numIters: Int)(f: => Unit): Unit = {
+    var i = 0
+    while (i < numIters) {
+      f
+      i += 1
+    }
+  }
+
+  /**
+   * Timing method based on iterations that permit JVM JIT optimization.
+   * @param numIters number of iterations
+   * @param f function to be executed. If prepare is not None, the running time of each call to f
+   *          must be an order of magnitude longer than one millisecond for accurate timing.
+   * @param prepare function to be executed before each call to f. Its running time doesn't count.
+   * @return the total time across all iterations (not couting preparation time)
+   */
+  def timeIt(numIters: Int)(f: => Unit, prepare: Option[() => Unit] = None): Long = {
+    if (prepare.isEmpty) {
+      val start = System.currentTimeMillis
+      times(numIters)(f)
+      System.currentTimeMillis - start
+    } else {
+      var i = 0
+      var sum = 0L
+      while (i < numIters) {
+        prepare.get.apply()
+        val start = System.currentTimeMillis
+        f
+        sum += System.currentTimeMillis - start
+        i += 1
+      }
+      sum
+    }
+  }
+  /**
+   * @param numIters Number of random numbers to generate while running the benchmark
+   * @return Map of execution times for { java.util.Random java.util.Random}
+   * and XORShift
+   */
+  def benchmark(numIters: Int): Map[String, Long] = {
+
+    val seed = 1L
+    val million = 1e6.toInt
+    val javaRand = new JavaRandom(seed)
+    val xorRand = new XORShiftRandom(seed)
+
+    // this is just to warm up the JIT - we're not timing anything
+    timeIt(million) {
+      javaRand.nextInt()
+      xorRand.nextInt()
+    }
+
+    /* Return results as a map instead of just printing to screen
+    in case the user wants to do something with them */
+    Map("javaTime" -> timeIt(numIters) { javaRand.nextInt() },
+      "xorTime" -> timeIt(numIters) { xorRand.nextInt() })
+  }
+}
 /**
  * Entry in vocabulary 
  */
@@ -405,8 +531,13 @@ object Word2vec {
         " to avoid an OOM. You are highly recommended to make your vocabSize*vectorSize," +
         "which is " + vocabSize + "*" + VectorSize + " for now, less than `Int.MaxValue/8`.")
     }
-    
-    
+
+    val initRandom = new XORShiftRandom(seed)
+    val syn0Global =
+      Array.fill[Float](vocabSize * vectorSize)((initRandom.nextFloat() - 0.5f) / vectorSize)
+    val syn1Global = new Array[Float](vocabSize * vectorSize)
+    val learningRate = resultingParameters.get[Double](LearningRate)
+    var alpha = learningRate
   }
   /**
    * trains network 
