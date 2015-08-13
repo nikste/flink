cd1b9305f75a4df6b4ea938ad92295f8ee4c2b1e
Nikolaas Steenbergen <nikolaas.steenbergen@googlemail.com>
2015-08-13 03:17:52 -0700

    Added streaming support for Flink-Scala-Shell

--- flink-staging/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/api/environment/RemoteStreamEnvironment.java
+++ flink-staging/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/api/environment/RemoteStreamEnvironment.java
@@ -40,7 +40,7 @@ public class RemoteStreamEnvironment extends StreamExecutionEnvironment {
 
 	private final String host;
 	private final int port;
-	private final List<File> jarFiles;
+	public List<File> jarFiles;
 
 	/**
 	 * Creates a new RemoteStreamEnvironment that points to the master
@@ -103,7 +103,7 @@ public class RemoteStreamEnvironment extends StreamExecutionEnvironment {
 	 *            jobGraph to execute
 	 * @return The result of the job execution, containing elapsed time and accumulators.
 	 */
-	private JobExecutionResult executeRemotely(JobGraph jobGraph) throws ProgramInvocationException {
+	public JobExecutionResult executeRemotely(JobGraph jobGraph) throws ProgramInvocationException {
 		if (LOG.isInfoEnabled()) {
 			LOG.info("Running remotely at {}:{}", host, port);
 		}
