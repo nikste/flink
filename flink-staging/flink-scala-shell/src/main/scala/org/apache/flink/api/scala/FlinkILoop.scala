a15022d27ebb0bcb29b384a37e6aa6259fe7c797
Nikolaas Steenbergen <nikolaas.steenbergen@googlemail.com>
2015-08-24 01:30:47 -0700

    added Twitter-connector dependencies

--- flink-staging/flink-scala-shell/src/main/scala/org/apache/flink/api/scala/FlinkILoop.scala
+++ flink-staging/flink-scala-shell/src/main/scala/org/apache/flink/api/scala/FlinkILoop.scala
@@ -178,6 +178,11 @@ class FlinkILoop(
     }
   }
 
+  def resetInterpreter(): Unit = {
+    intp.virtualDirectory.clear()
+    addThunk()
+  }
+
   /**
    * Packages the compiled classes of the current shell session into a Jar file for execution
    * on a Flink cluster.
